db3832b2cdaf3722741e4f7c521b54b925c4b947
P. Christeas <xrg@linux.gr>
2015-10-21 03:37:14 +0300

    bds,vbb: remove protobuf dependency
    Also, add protobuf dependency for abb
    
    Commit by M. Levogiannis

--- demos/demos/apps/vbb/models.py
+++ demos/demos/apps/vbb/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.core import urlresolvers
 
-from demos.common.utils import config, crypto, enums, fields
+from demos.common.utils import config, enums, fields
 
 
 class Config(models.Model):
