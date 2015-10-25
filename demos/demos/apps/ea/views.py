bc5e63cb7f24bc09946e741f73df14edb03c3692
P. Christeas <xrg@linux.gr>
2015-10-25 21:30:19 +0200

    ea/views: rename list variable, for pyflakes
    Reusing a name defined outside this comprehension may allow
    bugs, as suggested by pyflakes.

--- demos/demos/apps/ea/views.py
+++ demos/demos/apps/ea/views.py
@@ -145,8 +145,8 @@ class CreateView(View):
             
             if request.is_ajax():
                 
-                q_options_list = [len(question_obj['__list_OptionC__'])
-                    for question_obj in election_obj['__list_Question__']]
+                q_options_list = [len(qo['__list_OptionC__'])
+                    for qo in election_obj['__list_Question__']]
                 
                 vc_type = 'votecode' \
                     if not election_obj['long_votecodes'] else 'l_votecode'
