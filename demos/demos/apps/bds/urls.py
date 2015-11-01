fd4fb40c19c6ce477d0d59bf782ce495a7191fb2
P. Christeas <xrg@linux.gr>
2015-11-02 00:29:35 +0200

    bds/urls: fix pattern not accepting two-digit election ids

--- demos/demos/apps/bds/urls.py
+++ demos/demos/apps/bds/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     url(r'^$', views.HomeView.as_view(), name='home'),
-    url(r'^manage/(?P<election_id>[A-Za-z0-9])/$', login_required(views.ManageView.as_view()), name='manage'),
+    url(r'^manage/(?P<election_id>[A-Za-z0-9]+)/$', login_required(views.ManageView.as_view()), name='manage'),
 )
 
 apipatterns = [
