0c89754e845c013eb3c65fd1ea3121c1f0d383fe
P. Christeas <xrg@linux.gr>
2015-11-02 00:35:03 +0200

    spec,settings: move crypto socket to /var/run/demos-voting
    As experienced in the field, a socket in /tmp won't work because
    of apache's PrivateTmp setting, which enhances safety by having
    a namespaced /tmp per process.
    
    /var/run is the politically-correct place to put that socket into.
    However, it needs setup through "tmpfiles", as /run is a volatile
    filesystem.

--- demos/demos/settings/base.py
+++ demos/demos/settings/base.py
@@ -233,7 +233,7 @@ DEMOS_CONFIG = {
         # CRYPTO_AF: e.g. 'AF_UNIX' or 'AF_INET' or 'AF_INET6'
         # CRYPTO_ADDR: e.g. '/tmp/demos-crypto.sock' or ('127.0.0.1', 8999)
         'CRYPTO_AF': 'AF_UNIX',
-        'CRYPTO_ADDR': '/tmp/demos-crypto.sock',
+        'CRYPTO_ADDR': '/run/demos-voting/demos-crypto.sock',
 
         # Performance settings, they affect CPU and RAM usage, etc
 
