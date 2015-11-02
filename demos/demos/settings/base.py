8e065172c543665e1dbbf29f5b147641e82e9cac
P. Christeas <xrg@linux.gr>
2015-11-02 23:31:21 +0200

    settings: tweak SPOOL_DIR defaults, cert directories
    As suggested by M. Levogiannis

--- demos/demos/settings/base.py
+++ demos/demos/settings/base.py
@@ -15,7 +15,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # all writable files may have this as a reference
-SPOOL_DIR = '/var/spool/demos-voting' # or os.path.dirname(BASE_DIR) in development
+SPOOL_DIR = '/var/spool/demos-voting'
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,6 +27,9 @@ SECRET_KEY = NO_SECRET_KEY_DEFINED
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 DEVELOPMENT = False      # would turn off security, enable DEBUG
+
+if DEVELOPMENT:
+     SPOOL_DIR = os.path.join(os.path.dirname(BASE_DIR), 'data')
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
@@ -254,13 +257,13 @@ DEMOS_CONFIG = {
         # Absolute filesystem path to the directory that will hold
         # RSA private-public key pairs (PEM file format)
 
-        'PKEY_ROOT': os.path.join(SPOOL_DIR, 'data/pkeys'),
+        'PKEY_ROOT': os.path.join(SPOOL_DIR, 'pkeys'),
     },
     'bds': {
 
         # Absolute filesystem path to the directory that will hold
         # ballots (TAR file format)
-        'BALLOT_ROOT': os.path.join(SPOOL_DIR, 'data/ballots'),
+        'BALLOT_ROOT': os.path.join(SPOOL_DIR, 'ballots'),
     },
 
     'abb': {
@@ -268,7 +271,7 @@ DEMOS_CONFIG = {
         # Absolute filesystem path to the directory that will hold
         # X.509 certificates (PEM file format)
         
-        'CERT_ROOT': os.path.join(SPOOL_DIR, 'data/certs'),
+        'CERT_ROOT': os.path.join(SPOOL_DIR, 'certs'),
 
         # Performance settings, they affect CPU and RAM usage, etc
         'BATCH_SIZE': 128,
