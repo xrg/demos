a5c6885731bd739856ef386a8286fff81110772e
P. Christeas <xrg@linux.gr>
2015-11-17 21:41:26 +0200

    protobuf: support version 2.5.0 and above
    - 3.0.0 or above is recommended
    - but 2.5.0 is most widely available, 3.0.0 is still under development
    - a shell script that automatically makes the necessary changes to support
      python 3 and protobuf 2.5.0 is included
    
    Conflicts:
    	.gitignore

--- demos/demos/apps/ea/tasks/cryptotools.py
+++ demos/demos/apps/ea/tasks/cryptotools.py
@@ -15,7 +15,8 @@ config = registry.get_config('ea')
 
 def gen_key(ballots, options):
     
-    req = crypto.CryptoRequest()
+    req = crypto._CryptoRequest()
+    req.cmd = crypto._CryptoRequest.KeyGen
     
     req.kg.ballots = ballots
     req.kg.options = options
@@ -26,7 +27,8 @@ def gen_key(ballots, options):
 
 def gen_ballot(key, ballots, options, number):
     
-    req = crypto.CryptoRequest()
+    req = crypto._CryptoRequest()
+    req.cmd = crypto._CryptoRequest.GenBallot
     
     req.gb.key.CopyFrom(key)
     req.gb.ballots = ballots
@@ -41,7 +43,8 @@ def gen_ballot(key, ballots, options, number):
 
 def add_com(key, com_list):
     
-    req = crypto.CryptoRequest()
+    req = crypto._CryptoRequest()
+    req.cmd = crypto._CryptoRequest.AddCom
     
     req.ac.key.CopyFrom(key)
     req.ac.com.extend(com_list)
@@ -52,7 +55,8 @@ def add_com(key, com_list):
 
 def add_decom(key, decom_list):
     
-    req = crypto.CryptoRequest()
+    req = crypto._CryptoRequest()
+    req.cmd = crypto._CryptoRequest.AddDecom
     
     req.ad.key.CopyFrom(key)
     req.ad.decom.extend(decom_list)
@@ -63,7 +67,8 @@ def add_decom(key, decom_list):
 
 def complete_zk(key, options, coins, zk_list):
     
-    req = crypto.CryptoRequest()
+    req = crypto._CryptoRequest()
+    req.cmd = crypto._CryptoRequest.CompleteZK
     
     req.cz.key.CopyFrom(key)
     req.cz.options = options
@@ -80,7 +85,8 @@ def complete_zk(key, options, coins, zk_list):
 
 def verify_com(key, com, decom):
     
-    req = crypto.CryptoRequest()
+    req = crypto._CryptoRequest()
+    req.cmd = crypto._CryptoRequest.VerifyCom
     
     req.vc.key.CopyFrom(key)
     req.vc.com.CopyFrom(com)
@@ -90,7 +96,7 @@ def verify_com(key, com, decom):
     return res.check
 
 
-def _request_to_response(request, response_oneof):
+def _request_to_response(request, response_field):
     
     data = request.SerializeToString()
     size = intc.to_bytes(len(data), 4, 'big')
@@ -120,10 +126,10 @@ def _request_to_response(request, response_oneof):
     
     sock.close()
     
-    response = crypto.CryptoResponse()
+    response = crypto._CryptoResponse()
     response.ParseFromString(data)
     
-    if response.WhichOneof("response") != response_oneof:
+    if not response.HasField(response_field):
         raise RuntimeError("demos-crypto: invalid response")
     
     return response
